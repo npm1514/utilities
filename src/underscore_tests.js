227ba5fee7139f1dd047033c63a80f92ff524d4d
Nick Marucci <npm1514@gmail.com>
2015-12-09 13:31:14 -0700

    Day 1 progress

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -16,25 +16,50 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+    if(!n) {
+      return array[0];
+    }
+    else {
+      return array.slice(0,n);
+    }
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+    if(!n) {
+      return array[array.length -1];
+    }
+    else if(n > array.length) {
+      return array;
+    }
+    else {
+      return array.slice(array.length - n,array.length);
+    }
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
   _.each = function(collection, iterator) {
+    // for (var i = 0; i < collection.length, i++) {
+    //   iterator(collection[i], i, collection);      
+    // }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
   _.indexOf = function(array, target){
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] === target) {
+        return i;
+      }
+    }
+    return -1;
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+      for (var i = 0; i < )
   };
 
   // Return all elements of an array that don't pass a truth test.
